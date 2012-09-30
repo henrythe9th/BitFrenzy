3649f0d732b8d1438efd61875af2dc7ae62fe740
Henry <S>
2012-09-30 16:20:14 -0400

    Changed gameplay to be positive scoring and play forever

--- score.lua
+++ score.lua
@@ -1,11 +1,11 @@
 module(..., package.seeall)
 
-score = 30
-score_text = display.newText( score, 10, 0, native.systemFont, 24 )
+score = 0
+score_text = display.newText( "Current Score: "..score, 0, 0, native.systemFont, 18 )
 
 function update( val )
 	score = score + val
-	score_text.text = score
+	score_text.text = "Current Score: "..score
 end
 
 function isGameOver()
